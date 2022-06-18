68f508883c32723c2d0c993f0e01a80d0d2cee65
tvlad1234 <vlad.tomoiaga@gmail.com>
2022-06-18 23:27:39 +0300

    add drawLine function

--- Inc/gfx.h
+++ Inc/gfx.h
@@ -3,6 +3,7 @@
 
 #include "main.h"
 
+void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
 void drawCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
 void drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
 void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
--- Src/gfx.c
+++ Src/gfx.c
@@ -135,3 +135,53 @@ void printString(char s[]) {
 		writeChar(s[i]);
 }
 
+#ifndef _swap_int16_t
+#define _swap_int16_t(a, b)                                                    \
+  {                                                                            \
+    int16_t t = a;                                                             \
+    a = b;                                                                     \
+    b = t;                                                                     \
+  }
+#endif
+
+
+void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+                             uint16_t color) {
+
+  int16_t steep = abs(y1 - y0) > abs(x1 - x0);
+  if (steep) {
+    _swap_int16_t(x0, y0);
+    _swap_int16_t(x1, y1);
+  }
+
+  if (x0 > x1) {
+    _swap_int16_t(x0, x1);
+    _swap_int16_t(y0, y1);
+  }
+
+  int16_t dx, dy;
+  dx = x1 - x0;
+  dy = abs(y1 - y0);
+
+  int16_t err = dx / 2;
+  int16_t ystep;
+
+  if (y0 < y1) {
+    ystep = 1;
+  } else {
+    ystep = -1;
+  }
+
+  for (; x0 <= x1; x0++) {
+    if (steep) {
+      drawPixel(y0, x0, color);
+    } else {
+      drawPixel(x0, y0, color);
+    }
+    err -= dy;
+    if (err < 0) {
+      y0 += ystep;
+      err += dx;
+    }
+  }
+}
